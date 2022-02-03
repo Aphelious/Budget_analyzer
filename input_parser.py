a65983cc059d824942c4e03a1e6da8505cc11d21
Mike Powers <72277454+Aphelious@users.noreply.github.com>
2022-02-03 08:12:22 -0500

    Test commit for GitHub email change

--- input_parser.py
+++ input_parser.py
@@ -232,10 +232,6 @@ def parse_description(column):
     categorize_meta_description()
 
 
-
-
-
-
 def format_spreadsheet():
     create_new_cols()
     format_col_widths()
@@ -244,6 +240,11 @@ def format_spreadsheet():
     combine_debits_credits()
     delete_unused_cols()
     parse_description('H')
+
+
+def append_to_master_xlsx():
+    with load_workbook():
+        pass
 
 
 
